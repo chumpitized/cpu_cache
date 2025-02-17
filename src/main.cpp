a6866cf97b5c8b9e0bfa5be7e2c567fe37334789
chumpitized <126842502+chumpitized@users.noreply.github.com>
2025-02-17 09:31:12 -0600

    basic raylib setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,30 @@
+#include "raylib.h"
 #include <iostream>
+#include <vector>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+
+std::vector<u16> cache(4);
+std::vector<u8> memory(16);
 
 int main() {
 
-	std::cout << "hi" << std::endl;
+	SetTargetFPS(60);
+	InitWindow(500, 500, "CPU Cache");
+
+	while (!WindowShouldClose()) {
+
+		BeginDrawing();
+			ClearBackground(RAYWHITE);
+			DrawFPS(0, 0);
+			DrawRectangle(225, 225, 50, 50, BLACK);
+		EndDrawing();
+
+	}
+
+	CloseWindow();
+
+	return 1;
 
 }
