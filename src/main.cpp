c7b74dce51a8393813f2d6435f0c08c52d1c2c36
chumpitized <126842502+chumpitized@users.noreply.github.com>
2025-02-17 12:20:00 -0600

    beautify and draw actual hex values for memory and cache arrays

--- src/main.cpp
+++ src/main.cpp
@@ -12,41 +12,51 @@ void draw_memory() {
 	int x_offset = 50;
 	int y_offset = 100;
 
+	DrawText("Memory", x_offset, (y_offset - 50) / 2, 50, BLACK);
+
 	int width = 200;
 	int height = 50;
 
-	int font_y_offset = 5;
-	//int font_x_offset = GET TEXT WIDTH FIRST
-	int font_height = 40;
+	int font_size = 30;
+	int font_y_offset = (height - font_size) / 2;
+	int font_x_offset = x_offset + (width - MeasureText("0x00", font_size)) / 2;
 
 	for (int i = 0; i < memory.size(); ++i) {
 		int y_offset_inc = y_offset + (i * 50);
 
-		if (memory[i] & 1) DrawRectangle(x_offset, y_offset_inc, width, height, RED);
-		else DrawRectangle(x_offset, y_offset_inc, width, height, GREEN);
+		if (memory[i] & 1) DrawRectangle(x_offset, y_offset_inc, width, height, LIGHTGRAY);
+		else DrawRectangle(x_offset, y_offset_inc, width, height, GRAY);
 
-		DrawText("0x0", x_offset, y_offset_inc + font_y_offset, font_height, BLACK);
-	}	
+		char hex_buffer[9];
+		auto hex_code = sprintf(hex_buffer, "0x%02X", memory[i]);
+
+		DrawText(hex_buffer, font_x_offset, y_offset_inc + font_y_offset, font_size, BLACK);
+	}
 }
 
 void draw_cache() {
-	int x_offset = 700;
+	int x_offset = 750;
 	int y_offset = 100;
+
+	DrawText("Cache", x_offset, (y_offset - 50) / 2, 50, BLACK);
 
 	int width = 200;
 	int height = 50;
 
-	int font_y_offset = 5;
-	//int font_x_offset = GET TEXT WIDTH FIRST
-	int font_height = 40;
+	int font_size = 30;
+	int font_y_offset = (height - font_size) / 2;
+	int font_x_offset = x_offset + (width - MeasureText("0x00", font_size)) / 2;
 
 	for (int i = 0; i < cache.size(); ++i) {
 		int y_offset_inc = y_offset + (i * 50);
 
-		if (cache[i] & 1) DrawRectangle(x_offset, y_offset_inc, width, height, RED);
-		else DrawRectangle(x_offset, y_offset_inc, width, height, GREEN);
+		if (cache[i] & 1) DrawRectangle(x_offset, y_offset_inc, width, height, LIGHTGRAY);
+		else DrawRectangle(x_offset, y_offset_inc, width, height, GRAY);
 
-		DrawText("0x0", x_offset, y_offset_inc + font_y_offset, font_height, BLACK);
+		char hex_buffer[9];
+		auto hex_code = sprintf(hex_buffer, "0x%02X", memory[i]);
+
+		DrawText(hex_buffer, font_x_offset, y_offset_inc + font_y_offset, font_size, BLACK);
 	}
 }
 
@@ -66,7 +76,7 @@ int main() {
 	while (!WindowShouldClose()) {
 
 		BeginDrawing();
-			ClearBackground(RAYWHITE);
+			ClearBackground(YELLOW);
 			
 			draw_memory();
 			draw_cache();
