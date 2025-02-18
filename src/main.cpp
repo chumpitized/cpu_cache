c82932a154af91dd209af7623c4218775bdd2d15
chumpitized <126842502+chumpitized@users.noreply.github.com>
2025-02-18 16:33:22 -0600

    add instructions list and instruction pointer

--- src/main.cpp
+++ src/main.cpp
@@ -28,6 +28,8 @@ typedef struct Mem_Block {
 
 Color OFFWHITE = Color{220, 220, 220, 255};
 
+u8 instruction_pointer = 0;
+
 std::vector<Mem_Block> ram;
 std::vector<Mem_Block> cache;
 std::vector<Color> colors {
@@ -38,7 +40,7 @@ std::vector<Color> colors {
 };
 
 void fill_mem_array(int fill_size, u16 x_offset, u16 y_offset, u8 width, u8 height, std::vector<Mem_Block>& mem_array) {
-	for (u8 i = 0; i < fill_size; ++i) {
+	for (int i = 0; i < fill_size; ++i) {
 		Mem_Block block = Mem_Block {
 			.color = i & 1 ? RAYWHITE : OFFWHITE,
 
@@ -113,7 +115,7 @@ void try_select(Vector2 mouse_pos, int cache_size) {
 }
 
 //needs improvement
-void draw_lines() {
+void draw_lines(u16 cache_x_offset) {
 	for (int i = 0; i < ram.size(); ++i) {
 		Mem_Block mem = ram[i];
 		
@@ -122,7 +124,7 @@ void draw_lines() {
 			int y_end = 100 + (cache_index * 50) + 25;
 
 			Vector2 start_pos 	= Vector2{(float)mem.x_offset + mem.width - 5, (float)mem.y_offset + (mem.height / 2)};
-			Vector2 end_pos		= Vector2{(float)750, (float)y_end};
+			Vector2 end_pos		= Vector2{(float)cache_x_offset, (float)y_end};
 
 			DrawLineEx(start_pos, end_pos, 5, mem.color);
 		}
@@ -255,39 +257,82 @@ void draw_cache(u8 block_width) {
 	}
 }
 
-void draw_memory_name(const char* name, u16 x_offset, u16 y_offset, int font_size) {
+void draw_instruction_text(const char* text, int x_offset, int y_offset, int inst_box_width, int inst_box_height, int font_size) {
+	int text_width 	= MeasureText(text, font_size);
+	int adjusted_x 	= x_offset + ((inst_box_width - text_width) / 2);
+	int adjusted_y	= y_offset + ((inst_box_height - font_size) / 2);
+
+	DrawText(text, adjusted_x, adjusted_y, font_size, RED);
+}
+
+void draw_instructions(u16 x_offset, u16 y_offset, u16 inst_box_width, u16 inst_box_height, u16 inst_font_size) {
+	for (int i = 0; i < 20; ++i) {
+		int adjusted_y = y_offset + (i * inst_box_height);
+
+		if (i & 1) DrawRectangle(x_offset, adjusted_y, inst_box_width, inst_box_height, WHITE);
+		else DrawRectangle(x_offset, adjusted_y, inst_box_width, inst_box_height, LIGHTGRAY);
+
+
+		if (i == 0) {
+			draw_instruction_text("int a = 5", x_offset, adjusted_y, inst_box_width, inst_box_height, inst_font_size);
+		}
+
+		if (i == 1) {
+			draw_instruction_text("int b = 3", x_offset, adjusted_y, inst_box_width, inst_box_height, inst_font_size);
+		}
+
+		if (i == 2) {
+			draw_instruction_text("int c = a + b", x_offset, adjusted_y, inst_box_width, inst_box_height, inst_font_size);
+		}
+	}
+}
+
+void draw_instruction_pointer(float x_offset, float y_offset, float inst_box_width, float inst_box_height) {
+	DrawRectangleLinesEx(Rectangle{x_offset, y_offset + (instruction_pointer * inst_box_height), inst_box_width, inst_box_height}, 3, RED);
+}
+
+void draw_name(const char* name, u16 x_offset, u16 y_offset, int font_size) {
 	DrawText(name, x_offset, (y_offset - font_size) / 2, font_size, RAYWHITE);
 }
 
 int main() {
+	u16 inst_x_offset	= 700;
 	u16 ram_x_offset 	= 100;
-	u16 cache_x_offset	= 750; 
+	u16 cache_x_offset	= 400; 
 	u16 y_offset 		= 100;
 
 	u8 block_width 		= 200;
 	u8 block_height 	= 50;
 
+	u16 inst_box_width	= 300;
+	u16 inst_box_height	= 40;
+	u16 inst_font_size	= 30;
+
 	fill_mem_array(16, ram_x_offset, y_offset, block_width, block_height, ram);
 	fill_mem_array(4, cache_x_offset, y_offset, block_width, block_height, cache);
 
 	SetTargetFPS(60);
-	InitWindow(1000, 1000, "CPU Cache");
+	InitWindow(1050, 1000, "CPU Cache");
 
 	while (!WindowShouldClose()) {
 
 		BeginDrawing();
 			ClearBackground(Color{5, 5, 5, 255});
 			
-			draw_memory_name("RAM", ram_x_offset, y_offset, 50);
-			draw_memory_name("Cache", cache_x_offset, y_offset, 50);
+			draw_name("RAM", ram_x_offset, y_offset, 50);
+			draw_name("Cache", cache_x_offset, y_offset, 50);
+			draw_name("Instructions", inst_x_offset, y_offset, 50);
+
 
 			draw_ram(block_width);
 			draw_cache(block_width);
+			draw_instructions(inst_x_offset, y_offset, inst_box_width, inst_box_height, inst_font_size);
+			draw_instruction_pointer(inst_x_offset, y_offset, inst_box_width, inst_box_height);
 			handle_mouse_click();
 
 			DrawFPS(0, 0);
 
-			draw_lines();
+			draw_lines(cache_x_offset);
 		EndDrawing();
 
 	}
