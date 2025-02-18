1f140f2e866ae48406e0b233597827536c07ca77
chumpitized <126842502+chumpitized@users.noreply.github.com>
2025-02-18 09:05:20 -0600

    colorize cache and simplify some things

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,7 @@ typedef struct Mem_Block {
 Color OFFWHITE = Color{220, 220, 220, 255};
 
 std::vector<Mem_Block> memory;
-std::vector<u16> cache(4, 0);
+std::vector<Mem_Block> cache;
 std::vector<Color> colors {
 	ORANGE,
 	BLUE,
@@ -30,8 +30,8 @@ std::vector<Color> colors {
 	MAGENTA
 };
 
-void fill_memory(int cache_size, u8 x_offset, u8 y_offset, u8 width, u8 height) {
-	for (u8 i = 0; i < 16; ++i) {
+void fill_mem_array(int fill_size, u16 x_offset, u16 y_offset, u8 width, u8 height, std::vector<Mem_Block>& mem_array) {
+	for (u8 i = 0; i < fill_size; ++i) {
 		Mem_Block block = Mem_Block {
 			.color = i & 1 ? RAYWHITE : OFFWHITE,
 
@@ -46,28 +46,35 @@ void fill_memory(int cache_size, u8 x_offset, u8 y_offset, u8 width, u8 height) 
 			.value = 0,
 		};
 
-		memory.push_back(block);
+		mem_array.push_back(block);
 	}
 }
 
-
-
-void try_select_memory(Vector2 mouse_pos, int cache_size) {
+void try_select(Vector2 mouse_pos, int cache_size) {
 	for (int i = 0; i < memory.size(); ++i) {
 		Mem_Block& mem = memory[i];
 
 		if (mouse_pos.x > mem.x_offset && mouse_pos.x < mem.x_offset + mem.width && mouse_pos.y > mem.y_offset && mouse_pos.y <= mem.y_offset + mem.height) {
 			for (int j = 0; j < memory.size(); j++) {
-				Mem_Block& mem = memory[j];
+				int cache_index = j % cache_size;
 
-				if (j % cache_size == i % cache_size) {
-					mem.selected = true;
-					mem.color = colors[(j % cache_size)];
-					mem.width = 225;
+				Mem_Block& mem_block 	= memory[j];
+				Mem_Block& cache_line	= cache[cache_index];
+
+				if (cache_index == i % cache_size) {
+					mem_block.selected 	= true;
+					mem_block.color 	= colors[cache_index];
+					mem_block.width 	= 225;
+
+					cache_line.selected = true;
+					cache_line.color 	= colors[cache_index];
 				} else {
-					mem.selected = false;
-					mem.color = j & 1 ? RAYWHITE : OFFWHITE;
-					mem.width = 200;
+					mem_block.selected 	= false;
+					mem_block.color 	= j & 1 ? RAYWHITE : OFFWHITE;
+					mem_block.width 	= 200;
+
+					cache_line.selected = false;
+					cache_line.color 	= j & 1 ? RAYWHITE : OFFWHITE;
 				}
 
 			}
@@ -76,12 +83,17 @@ void try_select_memory(Vector2 mouse_pos, int cache_size) {
 		}
 	}
 
-
 	//if nothing selected, reset all memory
 	for (int i = 0; i < memory.size(); ++i) {
-		memory[i].color = i & 1 ? RAYWHITE : OFFWHITE;
-		memory[i].selected = false;
-		memory[i].width = 200;
+		Mem_Block& mem_block = memory[i];
+		Mem_Block& cache_line = cache[i % cache_size];
+
+		mem_block.color 	= i & 1 ? RAYWHITE : OFFWHITE;
+		mem_block.selected 	= false;
+		mem_block.width 	= 200;
+
+		cache_line.color 	= i & 1 ? RAYWHITE : OFFWHITE;
+		cache_line.selected = false;
 	}
 }
 
@@ -90,8 +102,8 @@ void draw_lines() {
 		Mem_Block mem = memory[i];
 		
 		if (mem.selected) {
-			Vector2 start_pos 	= Vector2{mem.x_offset + mem.width - 5, mem.y_offset + (mem.height / 2)};
-			Vector2 end_pos		= Vector2{750, 125};
+			Vector2 start_pos 	= Vector2{(float)mem.x_offset + mem.width - 5, (float)mem.y_offset + (mem.height / 2)};
+			Vector2 end_pos		= Vector2{(float)750, (float)125};
 
 			DrawLineEx(start_pos, end_pos, 5, mem.color);
 		}
@@ -102,10 +114,13 @@ void handle_mouse_click() {
 	if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
 		Vector2 mouse_pos = GetMousePosition();
 		
-		try_select_memory(mouse_pos, 4);
+		try_select(mouse_pos, 4);
 		//try_select_cache(mouse_pos);
 	}
 }
+
+//void draw_memory_name
+
 
 void draw_memory() {
 	int x_offset = 50;
@@ -148,24 +163,25 @@ void draw_cache() {
 	for (int i = 0; i < cache.size(); ++i) {
 		int y_offset_inc = y_offset + (i * 50);
 
-		if (i & 1) DrawRectangle(x_offset, y_offset_inc, width, height, LIGHTGRAY);
-		else DrawRectangle(x_offset, y_offset_inc, width, height, GRAY);
+		DrawRectangle(x_offset, y_offset_inc, cache[i].width, height, cache[i].color);
 
 		char hex_buffer[9];
-		auto hex_code = sprintf(hex_buffer, "0x%02X", cache[i]);
+		auto hex_code = sprintf(hex_buffer, "0x%02X", cache[i].value);
 
 		DrawText(hex_buffer, font_x_offset, y_offset_inc + font_y_offset, font_size, BLACK);
 	}
 }
 
 int main() {
-	u8 x_offset = 50;
-	u8 y_offset = 100;
+	u16 memory_x_offset = 50;
+	u16 cache_x_offset	= 750; 
+	u16 y_offset 		= 100;
 
-	u8 width = 200;
-	u8 height = 50;
+	u8 block_width 		= 200;
+	u8 block_height 	= 50;
 
-	fill_memory(4, x_offset, y_offset, width, height);
+	fill_mem_array(16, memory_x_offset, y_offset, block_width, block_height, memory);
+	fill_mem_array(4, cache_x_offset, y_offset, block_width, block_height, cache);
 
 	SetTargetFPS(60);
 	InitWindow(1000, 1000, "CPU Cache");
